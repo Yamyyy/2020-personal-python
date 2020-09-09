59268a3770857b61955e0f672285ec2e6abb912b
wish AH <XX_Ying@outlook.com>
2020-09-09 09:57:34 +0800

    [ADD]argument function
    [ADD]add some argument setting

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,1 +1,16 @@
-print( "Hello World!" )
+import argparse
+
+
+class Run:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.data = None
+        self.argInit()
+
+    def argInit(self):
+        self.parser.add_argument('-u','--user')
+        self.parser.add_argument('-e','--event')
+
+    def
+if __name__ == "__main__":
+    a = Run()
